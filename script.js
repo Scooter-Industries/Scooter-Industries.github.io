6e632b426c8cb1e80d810af6aa19afbe9f00946c
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-03-23 20:38:35 -0700

    Now with updating news

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $.get("https://fortnite-api.com/v2/news", function(data, status) {
+        if (status == "success"){
+            var lastUpdate = data["data"]["br"]["date"];
+            lastUpdate = lastUpdate.replaceAll("T"," ");
+            lastUpdate = lastUpdate.replaceAll("Z","");
+            $("#news").attr("src", data["data"]["br"]["image"]);
+            $("#lastUpdated").text("Last Updated: "+lastUpdate)
+        };
+    });
+});
